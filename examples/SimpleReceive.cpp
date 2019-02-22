b5b5bde1a90cd91bc4b16df33bf1ec39cd38f6f2
racsoraul <racsoraul@gmail.com>
2019-02-21 19:34:08 -0600

    Receiving position and rotation from agent 0.

--- examples/SimpleReceive.cpp
+++ examples/SimpleReceive.cpp
@@ -7,8 +7,9 @@
 #include <cstring>
 
 #if defined(__BORLANDC__) // workaround for BCB4 release build intrinsics bug
-namespace std {
-using ::__strcmp__;  // avoid error: E2316 '__strcmp__' is not a member of 'std'.
+namespace std
+{
+using ::__strcmp__; // avoid error: E2316 '__strcmp__' is not a member of 'std'.
 }
 #endif
 
@@ -16,66 +17,56 @@ using ::__strcmp__;  // avoid error: E2316 '__strcmp__' is not a member of 'std'
 #include "osc/OscPacketListener.h"
 #include "ip/UdpSocket.h"
 
+#define PORT 8000
 
-#define PORT 7000
-
-class ExamplePacketListener : public osc::OscPacketListener {
-protected:
-
-    virtual void ProcessMessage( const osc::ReceivedMessage& m, 
-				const IpEndpointName& remoteEndpoint )
+class ExamplePacketListener : public osc::OscPacketListener
+{
+  protected:
+    virtual void ProcessMessage(const osc::ReceivedMessage &m,
+                                const IpEndpointName &remoteEndpoint)
     {
-        (void) remoteEndpoint; // suppress unused parameter warning
+        (void)remoteEndpoint; // suppress unused parameter warning
 
-        try{
-            // example of parsing single messages. osc::OsckPacketListener
-            // handles the bundle traversal.
-            
-            if( std::strcmp( m.AddressPattern(), "/test1" ) == 0 ){
-                // example #1 -- argument stream interface
+        try
+        {
+            if (std::strcmp(m.AddressPattern(), "/agent/0/pos") == 0)
+            {
                 osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
-                bool a1;
-                osc::int32 a2;
-                float a3;
-                const char *a4;
-                args >> a1 >> a2 >> a3 >> a4 >> osc::EndMessage;
-                
-                std::cout << "received '/test1' message with arguments: "
-                    << a1 << " " << a2 << " " << a3 << " " << a4 << "\n";
-                
-            }else if( std::strcmp( m.AddressPattern(), "/test2" ) == 0 ){
-                // example #2 -- argument iterator interface, supports
-                // reflection for overloaded messages (eg you can call 
-                // (*arg)->IsBool() to check if a bool was passed etc).
-                osc::ReceivedMessage::const_iterator arg = m.ArgumentsBegin();
-                bool a1 = (arg++)->AsBool();
-                int a2 = (arg++)->AsInt32();
-                float a3 = (arg++)->AsFloat();
-                const char *a4 = (arg++)->AsString();
-                if( arg != m.ArgumentsEnd() )
-                    throw osc::ExcessArgumentException();
-                
-                std::cout << "received '/test2' message with arguments: "
-                    << a1 << " " << a2 << " " << a3 << " " << a4 << "\n";
+                float x, y, z;
+                args >> x >> y >> z >> osc::EndMessage;
+
+                std::cout << "pos from /agent/0: "
+                             "x = " << x << " y = " << y << " z = " << z << "\n";
             }
-        }catch( osc::Exception& e ){
-            // any parsing errors such as unexpected argument types, or 
+            else if (std::strcmp(m.AddressPattern(), "/agent/0/rot") == 0)
+            {
+                osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
+                float x, y, z;
+                args >> x >> y >> z >> osc::EndMessage;
+
+                std::cout << "rot from /agent/0: "
+                             "x = " << x << " y = " << y << " z = " << z << "\n";
+            }
+        }
+        catch (osc::Exception &e)
+        {
+            // any parsing errors such as unexpected argument types, or
             // missing arguments get thrown as exceptions.
             std::cout << "error while parsing message: "
-                << m.AddressPattern() << ": " << e.what() << "\n";
+                      << m.AddressPattern() << ": " << e.what() << "\n";
         }
     }
 };
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    (void) argc; // suppress unused parameter warnings
-    (void) argv; // suppress unused parameter warnings
+    (void)argc; // suppress unused parameter warnings
+    (void)argv; // suppress unused parameter warnings
 
     ExamplePacketListener listener;
     UdpListeningReceiveSocket s(
-            IpEndpointName( IpEndpointName::ANY_ADDRESS, PORT ),
-            &listener );
+        IpEndpointName(IpEndpointName::ANY_ADDRESS, PORT),
+        &listener);
 
     std::cout << "press ctrl-c to end\n";
 
@@ -83,4 +74,3 @@ int main(int argc, char* argv[])
 
     return 0;
 }
-
