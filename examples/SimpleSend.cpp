b1c5fa21706b2997a6521532debf93dfb8ef93b5
racsoraul <racsoraul@gmail.com>
2019-02-26 16:38:12 -0600

    Accepts addres and serialized json as command line arguments.

--- examples/SimpleSend.cpp
+++ examples/SimpleSend.cpp
@@ -16,41 +16,45 @@ using json = nlohmann::json;
 
 int main(int argc, char *argv[])
 {
-    (void)argc; // suppress unused parameter warnings
-    (void)argv; // suppress unused parameter warnings
+  (void)argc; // suppress unused parameter warnings
+  (void)argv; // suppress unused parameter warnings
 
-    UdpTransmitSocket transmitSocket(IpEndpointName(ADDRESS, PORT));
+  UdpTransmitSocket transmitSocket(IpEndpointName(ADDRESS, PORT));
 
-    char buffer[OUTPUT_BUFFER_SIZE];
-    osc::OutboundPacketStream p(buffer, OUTPUT_BUFFER_SIZE);
+  char buffer[OUTPUT_BUFFER_SIZE];
+  osc::OutboundPacketStream p(buffer, OUTPUT_BUFFER_SIZE);
 
-    // json config = {
-    //     {"id", 0},
-    //     {"port", 8000},
-    //     {"location", {
-    //             {"x", 0}, {"y", 0}, {"z", 0}
-    //         }
-    //     },
-    //     {"rotation", {
-    //             {"x", 0}, {"y", 0}, {"z", 0}
-    //         }
-    //     }
-    // };
+  // json config = {
+  //     {"id", 0},
+  //     {"port", 8000},
+  //     {"location", {
+  //             {"x", 0}, {"y", 0}, {"z", 0}
+  //         }
+  //     },
+  //     {"rotation", {
+  //             {"x", 0}, {"y", 0}, {"z", 0}
+  //         }
+  //     }
+  // };
 
-    //std::string configSerialized = config.dump();
-    std::string configSerialized = argv[1];
+  //std::string configSerialized = config.dump();
+
+  if (argc >= 3)
+  {
+    std::string spawningAddress = std::string("/spawn/") + argv[1];
+    std::string configSerialized = argv[2];
 
     // Sending configuration for Spawning
     p << osc::BeginBundle();
-    p << osc::BeginMessage("/spawn/quadcopter")
+    p << osc::BeginMessage(spawningAddress.c_str())
       << osc::Blob(configSerialized.c_str(), configSerialized.length()) << osc::EndMessage
       << osc::EndBundle;
 
-    // Sending velocity
-    //   p << osc::BeginBundleImmediate
-    //     << osc::BeginMessage("/agent/0/velocity/xy")
-    //     << -0.52 << 0.37 << osc::EndMessage
-    //     << osc::EndBundle;
-
     transmitSocket.Send(p.Data(), p.Size());
+  } else
+  {
+    std::cout << "ERROR: Not enough arguments have been passed!" << "\n"
+              << "USAGE: ./SimpleSend ADDRESS SPAWN_JSON" << std::endl;
+  }
+  
 }
