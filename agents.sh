7767d2d6dd60a8e78f3fcbac8ce091bf6f6cbf0d
racsoraul <racsoraul@gmail.com>
2019-02-26 16:44:31 -0600

    agent.sh script accepts address, x_separation and z_init_position argument

--- agents.sh
+++ agents.sh
@@ -1,5 +1,38 @@
 #!/usr/bin/env bash
-for agent in {1..100}
+address=$1
+x_separation=$2
+z_init_position=$3
+
+# Address argument is required
+if [ -z "$address" ]
+then
+  echo
+  echo "ERROR! Address argument is required."
+  echo "USAGE: agent.sh ADDRESS [X_SEPARATION] [Z_INIT_POSITION]"
+  echo
+  exit 1
+fi
+
+# Default X separation
+if [ -z "$x_separation" ]
+then
+ x_separation=50
+fi
+
+# Default Y initial position
+if [ -z "$z_init_position" ]
+then
+ z_init_position=50
+fi
+
+echo
+echo "=== Script running with following values ==="
+echo "Address: $address"
+echo "X separation units: $x_separation"
+echo "Z initial position: $z_init_position"
+echo
+
+for agent in {0..50}
 do
-  ./bin/SimpleSend "{\"id\":$agent,\"port\":$((8000+agent)),\"location\":{\"x\":$((agent * 50)),\"y\":50,\"z\":0},\"rotation\":{\"x\":0,\"y\":0,\"z\":0}}"
+  ./bin/SimpleSend "$address" "{\"id\":$agent,\"port\":$((8000+agent)),\"location\":{\"x\":$((agent * x_separation)),\"y\":50,\"z\":$z_init_position},\"rotation\":{\"x\":0,\"y\":0,\"z\":0}}"
 done
